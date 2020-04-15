ffa32d666dcf9ccd2102d3616b57a10fde38a093
webdesc <webdesc@gmail.com>
2020-04-15 10:45:03 +0300

    move fixtures

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -3,7 +3,7 @@ import path from 'path';
 import ConfigFactory from '../src/ConfigFactory';
 import gendiff from '../src';
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__tests__', '__fixtures__', filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 const plainEqualsResult = 'host: hexlet.io\n- timeout: 50\n+ timeout: 20\n- proxy: 123.234.53.22\n- follow: false\n+ verbose: true';
 
